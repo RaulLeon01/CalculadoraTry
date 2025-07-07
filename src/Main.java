926d73f046df7bab23f2692048744b9464527ff0
RaulLeon01 <20243rd013@utez.edu.mx>
2025-07-07 08:33:09 -0600

    Declaración Variables

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Variables
+        double num1;
+        double num2;
+        double result;
+
+        //
+        
+    }
+    public void capturaNum(){
+        Scanner captura = new Scanner(System.in);
+        System.out.println("Ingrese el primer numero: ");
+        int num1 = captura.nextInt();
+        System.out.println("Ingrese el segundo numero: ");
+        int num2 = captura.nextInt();
+    }
+
+
+}
